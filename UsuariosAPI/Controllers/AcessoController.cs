18653d2d26341a32e6ac8a774b1c3efa7d7b526a
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-23 20:46:10 -0300

    Criado o controlador de acesso

---
+++ UsuariosAPI/Controllers/AcessoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosAPI.Controllers
+{
+    //validador de acesso a partir do token
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class AcessoController: ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Policy = "IdadeMinima")]
+        public ActionResult Get() 
+        {
+            return Ok("Acesso permitido");
+        }
+    }
+}
