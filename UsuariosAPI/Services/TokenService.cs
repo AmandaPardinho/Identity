1f68a36ff680318c1e57acd1a475a1d1f523cf7d
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-23 18:23:56 -0300

    Serviço com o código para gerar um token para o usuário

---
+++ UsuariosAPI/Services/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Services
+{
+    public class TokenService
+    {
+        public string GenereteToken(Usuario usuario)
+        {
+            Claim[] claims = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id),
+                new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hjdsgfgsfhJSVFI96154SNHJFBBCKHJVGJ"));
+
+            var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+                (
+                expires: DateTime.Now.AddMinutes(10),
+                claims: claims,
+                signingCredentials: signingCredentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
